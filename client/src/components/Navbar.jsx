32fee344d3d34404997722d26fb4fac4645950ce
Saru <vauntadie@gmail.com>
2026-06-03 21:25:34 +0545

    frontend structure initiated

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React, { useContext } from 'react';
+import {link, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../context/AuthContext';
+import { FaTicketAlt } from 'react-icons/fa';
+
+const Navbar = () => {
+    
+
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
---
+++ client/src/context/AuthContext.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+export const AuthContext = React.createContext();
+
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = React.useState(null);
+    const [loading, setLoading] = React.useState(true);
+
+    React.useEffect(() => {
+        const storedUser = localStorage.getItem("user");
+        if (storedUser) {
+            setUser(JSON.parse(storedUser));
+        }
+        setLoading(false);
+    }, []);
+
+    const login = async (email, password) => {
+       try{
+        const {data} = await api.post('/auth/login', {email, password});
+        setUser(data);
+        localStorage.setItem("user", JSON.stringify(data));
+        localStorage.setItem("token", data.token);
+        return data;
+       }
+       catch(err){
+        console.log("Login failed:", err);
+        throw err;
+       }
+    };
+
+    const register = async (name, email, password) => {
+         try{
+            const {data} = await api.post('/auth/register', {name, email, password});
+            setUser(data);
+            return data;
+         }
+         catch (err){
+            console.log("Registration failed:", err);
+            throw err;
+         }
+    }
+
+    const verifyOTP = async () => {
+        try{
+            const {data} = await api.post('/auth/verify-otp');
+            setUser(Data);
+            localStorage.setItem("user", JSON.stringify(data));
+            localStorage.setItem("token", data.token);
+            return data;
+        }
+        catch(err) {
+            console.log("OTP verification failed", err);
+            throw err;
+        }
+    };
+
+    const logout = () => {
+        setUser(null);
+        localStorage.removeItem("user");
+        localStorage.removeItem("token");
+    };
+
+    return (
+        <AuthContext.Provider value={{ user, loading, login, logout, verifyOTP, register}}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
